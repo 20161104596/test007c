73498048563c5ccee3cbfcc268d4bdd9f250c9a7
20161104596 <964312086@qq.com>
2017-06-07 08:46:16 +0800

    Initial Commit

---
+++ test007c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test007c
+//
+//  Created by 20161104596 on 17/6/7.
+//  Copyright © 2017年 20161104596. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
